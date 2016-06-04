8787fdb73ad795f7692599c47160901f138461a2
julrusak <juliannarusak@gmail.com>
2016-06-03 22:21:36 -0400

    Moving magnet poem pieces

--- app/assets/javascripts/magnet_poems.js
+++ app/assets/javascripts/magnet_poems.js
@@ -1,3 +1,31 @@
 $( document ).ready(function(){
-	$('.word').draggable();
-}); 
+	$('.draggable').draggable({
+	    revert:"invalid",
+	    onStartDrag:function(){
+	        $(this).draggable('options').cursor = 'not-allowed';
+	        $(this).draggable('proxy').css('z-index',10);
+	    },
+	    onStopDrag:function(){
+	        $(this).draggable('options').cursor='move';
+	    }
+	});
+
+	$('#droppable').droppable({
+	    onDragEnter:function(e,source){
+	        $(source).draggable('options').cursor='auto';
+	    },
+	    onDragLeave:function(e,source){
+	        $(source).draggable('options').cursor='not-allowed';
+	    },
+	    onDrop:function(e,source){
+	        var word = $(source).html();
+	        addWord(word);
+	    }
+	});
+	var poem = {"words":[]};
+	function addWord(name){
+        poem.push(word);
+	    $('#cartcontent').datagrid('loadData', poem);
+	}    
+
+});
